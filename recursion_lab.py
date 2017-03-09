ab60281b3ddb3427d89c95719acfd9afb100e031
sdemirjian <sdemirjian@fwparker.org>
2017-03-09 16:06:10 -0600

    finished function problems

--- recursion_lab.py
+++ recursion_lab.py
@@ -9,3 +9,29 @@ to change the appearance and behavior.
 Give your fractal a depth of at least 5.  Ensure the fractal is contained on the screen (at whatever size you set it).  Have fun.
 (35pts)
 '''
+import turtle
+
+#Before you run: You can close the turtle window at any time. I made so many recursions because without it it wouldn't form such a nice design. Enjoy!
+
+my_turtle = turtle.Turtle()
+my_screen = turtle.Screen()
+
+color_list = ["red", "orange", "yellow", "green", "blue", "purple", "brown", "grey", "black"]
+
+def fractal(x, y, heading, dist, depth):
+    my_turtle.up()
+    my_turtle.goto(x,y)
+    my_turtle.color(color_list[depth%len(color_list)])
+    my_turtle.down()
+    my_turtle.setheading(heading)
+    my_turtle.forward(dist)
+
+    new_y = my_turtle.ycor()
+    new_x = my_turtle.xcor()
+
+    if depth > 0:
+        fractal(new_x, new_y, heading + 10, dist/1.2, depth - 1)
+        fractal(new_x, new_y, heading - 50, dist/1.2, depth - 1)
+
+my_turtle.speed(0)
+fractal(-250,-220,45,100,12)
--- recursion_problem_set.py
+++ recursion_problem_set.py
@@ -4,14 +4,47 @@ Create a single recursive function (or more if you wish), which can answer the f
 '''
 
 #1.  You have $10000 on a high interest credit card with an APR of 20.0% (calculated MONTHLY, so MPR is APR/12).  Assuming you make no payments for 6 months, what is your new balance?  Solve recursively.
+def interest(starting_amount, interest_rate, depth):
+    new_amount = starting_amount + (starting_amount * interest_rate)
+
+    if depth > 0:
+        interest(new_amount, interest_rate, depth - 1)
+    else:
+        print(new_amount)
+
+interest(10000, .20/12, 6)
+print()
 
 #2.  You have $5000 on a high interest credit card with an APR of 20.0% (calculated MONTHLY).  You make the minimum payment of $100 per month for 36 months.  What is your new balance?  Solve recursively.
+def interest(starting_amount, interest_rate, depth):
+    new_amount = starting_amount + ((starting_amount + 100) * interest_rate)
+
+    if depth > 0:
+        interest(new_amount, interest_rate, depth - 1)
+    else:
+        print(new_amount)
+interest(5000, .20/12, 36)
+print()
 
 #3.  You have $10000 on a high interest credit card with an APR of 20.0% (calculated MONTHLY).  If you make the minimum payment of $100 per month, how many months will it take to pay it off?  Solve recursively.
+def interest(starting_amount, interest_rate, months):
+    new_amount = starting_amount + ((starting_amount + 100) * interest_rate)
 
-
-
-
-
+    try:
+        if new_amount != 0:
+            interest(new_amount, interest_rate, months + 1)
+    except:
+        print("You've exceeded the amount. You spend too much!")
+        print("At the end of ", months, "months, you had", new_amount, "monies")
+interest(10000, .20/12, 0)
+print()
 
 #4  Pyramid of Cubes - (10pts) If you stack boxes in a pyramid, the top row would have 1 box, the second row would have two, the third row would have 3 and so on.  Make a recursive function which calculates the TOTAL NUMBER OF BOXES for a pyramid of boxes n high.  For instance, a pyramid that is 3 high would have a total of 6 boxes.  A pyramid 4 high would have 10.
+def pyramid(starting_boxes, n, x):
+    total_boxes = starting_boxes + x
+
+    if n > 1:
+        pyramid(total_boxes, n - 1, x + 1)
+    else:
+        print(total_boxes)
+pyramid(0, int(input("How tall is the pyramid?")), 1)
